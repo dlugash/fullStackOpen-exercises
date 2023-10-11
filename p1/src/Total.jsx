f86620349f93fac9359e845935976b14b60d8849
dlugash <lera.dlugash@gmail.com>
2023-10-11 23:08:03 +0300

    exercise 1.4 done

--- p1/src/Total.jsx
+++ p1/src/Total.jsx
@@ -1,8 +1,9 @@
 const Total = (props) => {
-  const {exercises1, exercises2, exercises3} = props;
+  const exercises = props.parts.map(part => part.exercises);
+  const sum = exercises.reduce((ex, ex1) => ex + ex1)
   return (
     <p>
-      Number of exercises {exercises1 + exercises2 + exercises3}
+      Number of exercises {sum}
     </p>
   )
 }
